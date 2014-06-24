59089780da437235f0ccf46f737173473d358de0
Maxim Gorshkov <maxim.gorshkov@savoirfairelinux.com>
2014-06-24 12:09:14 -0400

    Main Panel.

---
+++ COMP396-REVIEWS/src/com/GUI/Driver.java
@@ -0,0 +1,15 @@
+package com.GUI;
+
+/**
+ * Driver which intialises the full project.
+ * @author mkgo
+ * @version 0.1
+ * 
+ */
+public class Driver {
+	public static void main(String[] args) {
+		//Initialize the main window
+		MainFrame m = new MainFrame();
+		m.setVisible(true);
+	}
+}
---
+++ COMP396-REVIEWS/src/com/GUI/MainFrame.java
@@ -0,0 +1,15 @@
+package com.GUI;
+
+import javax.swing.JFrame;
+
+/**
+ * Main window responsible for holding the container.
+ * @author mkgo
+ *
+ */
+public class MainFrame extends JFrame{
+	public MainFrame(){
+		setTitle("Article Quality Sorter");
+		setSize(300,200);
+	}
+}
